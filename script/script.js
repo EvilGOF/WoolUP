b640e922c7ad3b0b3d57cfe500c82e3976c4641c
Guido Fernandez <guidofernandez98@gmail.com>
2022-05-01 22:46:43 -0300

    agregando arrays

--- script/script.js
+++ script/script.js
@@ -1,12 +1,7 @@
 function user(nombre, localidad) {
   this.nombre = nombre;
+
   this.localidad = localidad;
-}
-
-function articulo(nombreArticulo, precioArticulo) {
-  this.nombreArticulo = nombreArticulo;
-
-  this.precioArticulo = precioArticulo;
 }
 
 function calculoImporteFinal(articulo, envio, iva) {
@@ -19,23 +14,16 @@ function saludoImporte(importeFinal) {
   alert(`El importe final es de : $` + importeFinal + `\nDisfrute su compra!`);
 }
 
-function menuArticulos() {
-  alert(
-    `\n ${articulo1.nombreArticulo} : $${articulo1.precioArticulo} \n ${articulo2.nombreArticulo} : $${articulo2.precioArticulo} \n ${articulo3.nombreArticulo} : $${articulo3.precioArticulo} \n ${articulo4.nombreArticulo} : $${articulo4.precioArticulo} \n ${articulo5.nombreArticulo} : $${articulo5.precioArticulo} \n ${articulo6.nombreArticulo} : $${articulo6.precioArticulo}`
-  );
-}
+const listaArticulos = [
+  { idArticulo: 1, nombreArticulo: "Alfombra", precioArticulo: 500 },
+  { idArticulo: 2, nombreArticulo: "Bufanda", precioArticulo: 400 },
+  { idArticulo: 3, nombreArticulo: "Guantes", precioArticulo: 200 },
+  { idArticulo: 4, nombreArticulo: "Gorro", precioArticulo: 500 },
+  { idArticulo: 5, nombreArticulo: "Manta", precioArticulo: 1000 },
+  { idArticulo: 6, nombreArticulo: "Canasta", precioArticulo: 600 },
+];
 
-const articulo1 = new articulo("Alfombra", 500);
-
-const articulo2 = new articulo("Bufanda", 400);
-
-const articulo3 = new articulo("Guantes", 200);
-
-const articulo4 = new articulo("Gorro", 500);
-
-const articulo5 = new articulo("Manta", 1000);
-
-const articulo6 = new articulo("Canasta", 600);
+const carrito = [];
 
 let nombre = prompt("Ingrese su nombre:");
 
@@ -43,30 +31,62 @@ let localidad = prompt("Ingrese su localidad:");
 
 const name = new user(nombre, localidad);
 
-let menu = menuArticulos();
-
 let entrada = prompt(
-  `Bienvenido ${nombre}! \nIngrese el importe del artículo. Ingrese ESC si desea salir del programa.`
+  `Bienvenido ${nombre}! \nIngrese el ID del artículo deseado.  
+  \nId 1 Alfombra: $500 
+  \nId 2 Bufanda: $400 
+  \nId 3 Guantes: $200
+  \nId 4 Gorro: $500 
+  \nId 5 Manta: $1000 
+  \nId 6 Canasta: $600 
+  \nIngrese ESC si desea salir del programa.`
 );
 
 while (entrada != "ESC") {
   let envio;
 
-  if (localidad == "CABA") {
+  if (localidad === "CABA") {
     envio = 200;
   } else {
     envio = 300;
   }
 
-  let articulo = parseInt(entrada);
+  entrada = parseInt(entrada);
 
-  let importeFinal = calculoImporteFinal(articulo, envio, iva(articulo));
+  const elementoCarrito = listaArticulos.find((i) => i.idArticulo === entrada);
+
+  carrito.push(elementoCarrito.precioArticulo);
+
+  let alertCarrito = "";
+
+  for (const [key, value] of Object.entries({
+    Articulo: elementoCarrito.nombreArticulo,
+    Precio: "$" + elementoCarrito.precioArticulo,
+  })) {
+    alertCarrito += `${key}: ${value}\n`;
+  }
+  alert(`Felicidades, agregaste a tu carrito:\n${alertCarrito}`);
+
+  console.log(carrito);
+
+  const total = parseInt(carrito.reduce((j, i) => j + i, 0));
+
+  console.log(total);
+  console.log(envio);
+  console.log(iva(total));
+
+  let importeFinal = calculoImporteFinal(total, envio, iva(total));
 
   const saludo = saludoImporte(importeFinal);
 
-  menu = menuArticulos();
-
   entrada = prompt(
-    `Ingrese otro importe. O ingrese ESC si desea salir del programa.`
+    `Ingrese otro Id para agregar un articulo.
+    \nId 1 Alfombra: $500 
+    \nId 2 Bufanda: $400 
+    \nId 3 Guantes: $200
+    \nId 4 Gorro: $500 
+    \nId 5 Manta: $1000 
+    \nId 6 Canasta: $600 
+    \nO ingrese ESC si desea salir del programa.`
   );
 }
