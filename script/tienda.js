592e5d66af45e494bd5d58614a60ec10c32345bf
Guido Fernandez <guidofernandez98@gmail.com>
2022-06-01 18:58:24 -0300

    fix

--- script/tienda.js
+++ script/tienda.js
@@ -12,54 +12,6 @@ const templateCarrito = document.getElementById("template-carrito").content;
 
 const fragment = document.createDocumentFragment();
 
-const form = document.querySelector("#formulario");
-
-const enviarFormulario = (event) => {
-  event.preventDefault();
-
-  const { name, email } = event.target;
-
-  if (name.value.length === 0 || email.value.length === 0) {
-    Swal.fire({
-      icon: "error",
-      title: "Oops...",
-      text: "Ingrese su nombre y mail!",
-    });
-  } else {
-    Swal.fire({
-      title: `Bienvenido ${name.value}! Enviaremos su factura a ${email.value}`,
-      showClass: {
-        popup: "animate__animated animate__fadeInDown",
-      },
-      hideClass: {
-        popup: "animate__animated animate__fadeOutUp",
-      },
-    });
-
-    localStorage.setItem("name", name.value);
-
-    localStorage.setItem("email", email.value);
-
-    form.innerHTML = "";
-
-    const title = document.getElementById("title-gallery");
-
-    title.innerHTML = `Bienvenido ${name.value} !`;
-  }
-};
-
-if (localStorage.getItem("name")) {
-  const name = localStorage.getItem("name");
-
-  form.innerHTML = "";
-
-  const title = document.getElementById("title-gallery");
-
-  title.innerHTML = `Bienvenido ${name} !`;
-}
-
-form.addEventListener("submit", enviarFormulario);
-
 let carrito = {};
 
 document.addEventListener("DOMContentLoaded", () => {
